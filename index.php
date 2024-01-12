fc7f25f46dc18cd2f328a8967870083645e3adb0
dipannita25 <dipannitaraul.laboni@gmail.com>
2024-01-12 13:03:38 +0530

    up

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php include 'includes/header.php' ?>
 
+<!-- banner part -->
+
 <section class="banner">
     <div class="container">
         <div class="swiper mySwiper">
@@ -22,7 +24,7 @@
                 <div class="swiper-slide">
                     <div class="banner-slide">
                         <div class="left-bannercnt">
-                            <p>Restoring Hope And Health  <br>For Over Two Decades
+                            <p>Restoring Hope And Health <br>For Over Two Decades
                             </p>
                             <h4>Where matters of the <br> heart are understood <br>the best </h4>
                             <a href="#">Book An
@@ -55,6 +57,111 @@
 
     </div>
 </section>
+
+<!-- centers of excellence -->
+
+<section class="HospitalAbt">
+    <div class="custContainer">
+        <div class="HospitalAbtTop">
+            <h5>Centres Of Excellence</h5>
+            <p>Combining the best specialists and equipment to provide you nothing short of the best in healthcare</p>
+        </div>
+        <div class="HospitalAbtBtm">
+            <div class="row">
+                <div class="col-md-3">
+                    <!-- Tabs nav -->
+                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                        <button class="nav-link tabBtnLink active show" id="v-pills-Heart-tab" data-toggle="pill" href="#v-pills-Heart" role="tab" aria-controls="v-pills-Heart" aria-selected="true"><img src="./assets/images/heart.png" alt="heart">
+                            <span>Heart</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Spine-tab" data-toggle="pill" href="#v-pills-Spine" role="tab" aria-controls="v-pills-Spine" aria-selected="false"><img src="./assets/images/spine.png" alt="spine">
+                            <span>Spine</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Orthopedics-tab" data-toggle="pill" href="#v-pills-Orthopedics" role="tab" aria-controls="v-pills-Orthopedics" aria-selected="false"><img src="./assets/images/Orthopaedics.png" alt="Orthopaedics">
+                            <span>Orthopedics</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-CancerCare-tab" data-toggle="pill" href="#v-pills-CancerCare" role="tab" aria-controls="v-pills-CancerCare" aria-selected="false"><img src="./assets/images/Cancer.png" alt="Cancer">
+                            <span>Cancer Care</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Neurosciences-tab" data-toggle="pill" href="#v-pills-Neurosciences" role="tab" aria-controls="v-pills-Neurosciences" aria-selected="false"><img src="./assets/images/Neurosciences.png" alt="Neurosciences">
+                            <span>Neurosciences</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Gastroenterology-tab" data-toggle="pill" href="#v-pills-Gastroenterology" role="tab" aria-controls="v-pills-Gastroenterology" aria-selected="false"><img src="./assets/images/Gastroentrology.png" alt="Gastroentrology">
+                            <span>Gastroenterology</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Transplants-tab" data-toggle="pill" href="#v-pills-Transplants" role="tab" aria-controls="v-pills-Transplants" aria-selected="false"><img src="./assets/images/Transplant.png" alt="Transplant">
+                            <span>Transplants</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-CriticalCare-tab" data-toggle="pill" href="#v-pills-CriticalCare" role="tab" aria-controls="v-pills-CriticalCare" aria-selected="false"><img src="./assets/images/critical care.png" alt="critical care">
+                            <span>Critical Care</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Bariatricsurgery-tab" data-toggle="pill" href="#v-pills-Bariatricsurgery" role="tab" aria-controls="v-pills-Bariatricsurgery" aria-selected="false"><img src="./assets/images/Bariatric surgery.png" alt="Bariatric surgery">
+                            <span>Bariatric Surgery</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Robotics-tab" data-toggle="pill" href="#v-pills-Robotics" role="tab" aria-controls="v-pills-Robotics" aria-selected="false"><img src="./assets/images/Robotics.png" alt="Robotics">
+                            <span>Robotics</span></button>
+
+                        <button class="nav-link tabBtnLink" id="v-pills-Medicine-tab" data-toggle="pill" href="#v-pills-Medicine" role="tab" aria-controls="v-pills-Medicine" aria-selected="false"><img src="./assets/images/Preventive Medicine.png" alt="Medicine">
+                            <span>Preventive Medicine</span></button>
+
+                    </div>
+                </div>
+
+
+                <div class="col-md-9">
+                    <!-- Tabs content -->
+                    <div class="tab-content" id="v-pills-tabContent">
+                        <div class="tab-pane active show" id="v-pills-Heart" role="tabpanel" aria-labelledby="v-pills-Heart-tab">
+                            <h4 class="font-italic mb-4">Heart</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Spine" role="tabpanel" aria-labelledby="v-pills-Spine-tab">
+                            <h4 class="font-italic mb-4">Spine</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Orthopedics" role="tabpanel" aria-labelledby="v-pills-Orthopedics-tab">
+                            <h4 class="font-italic mb-4">Orthopedics</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-CancerCare" role="tabpanel" aria-labelledby="v-pills-CancerCare-tab">
+                            <h4 class="font-italic mb-4">Cancer Care</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Neurosciences" role="tabpanel" aria-labelledby="v-pills-Neurosciences-tab">
+                            <h4 class="font-italic mb-4">Neurosciences</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Gastroenterology" role="tabpanel" aria-labelledby="v-pills-Gastroenterology-tab">
+                            <h4 class="font-italic mb-4">Gastroenterology</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Transplants" role="tabpanel" aria-labelledby="v-pills-Transplants-tab">
+                            <h4 class="font-italic mb-4">Transplants</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-CriticalCare" role="tabpanel" aria-labelledby="v-pills-CriticalCare-tab">
+                            <h4 class="font-italic mb-4">Critical Care</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Bariatricsurgery" role="tabpanel" aria-labelledby="v-pills-Bariatricsurgery-tab">
+                            <h4 class="font-italic mb-4">Bariatric Surgery</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Robotics" role="tabpanel" aria-labelledby="v-pills-Robotics-tab">
+                            <h4 class="font-italic mb-4">Robotics</h4>
+                        </div>
+
+                        <div class="tab-pane fade" id="v-pills-Medicine" role="tabpanel" aria-labelledby="v-pills-Medicine-tab">
+                            <h4 class="font-italic mb-4">Preventive Medicine</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <script>
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 1,
@@ -64,7 +171,7 @@
             delay: 2500,
             disableOnInteraction: false,
         },
-    
+
     });
 </script>
 
